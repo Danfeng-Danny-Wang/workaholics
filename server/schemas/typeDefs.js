79663c560b083ef9f58ef5a03980f2fd36f66976
Eric <esbev@hotmail.com>
2023-10-12 20:48:38 -0500

    fixed typDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -11,24 +11,29 @@ const typeDefs = gql`
     company: Company
   }
 
+  type Message {
+    username: String
+    text: String
+    timeStamp: String
+  }
+
+  input MessageInput {
+    username: String
+    text: String
+    timeStamp: String
+  }
+
+  type Room {
+    _id: ID
+    name: String
+    messages: [Message]
+  }
+
   type Company {
     _id: ID
     name: String
     code: String
     chatRooms: [Room]
-  }
-
-  type Room {
-    _id: _id
-    name: String
-    messages: [Message]
-  }
-
-  type Message {
-    _id: _id
-    username: String
-    text: String
-    timeStamp: String
   }
 
   type Auth {
@@ -38,16 +43,16 @@ const typeDefs = gql`
 
   type Query {
     user: User
-    company(chatRooms: [Room]): Company
-    room(messages: [Message]): Room
-    message: Message
+    company(chatRooms: String): Company
+    room(messages: String): Room
+    message: [Message]
   }
 
   type Mutation {
     loginUser(email: String!, password: String!): Auth
     addUser(firstName: String!, lastName: String!, username: String!, email: String!, password: String!, company: String!): Auth
     getCompany(name: String!, code: String!): Company
-    addMessage(name: String!, text: String, timeStamp: String): Message
+    addMessage(messageData: MessageInput!): Room
   }
 
 `;
