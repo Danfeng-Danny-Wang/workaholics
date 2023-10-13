8670b6ac8d39fd287fc48e1f3830a4fa55101f80
Eric <esbev@hotmail.com>
2023-10-12 21:27:14 -0500

    changes to typedefs and added some resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -43,16 +43,17 @@ const typeDefs = gql`
 
   type Query {
     user: User
-    company(chatRooms: String): Company
-    room(messages: String): Room
+    company: Company
+    room: Room
     message: [Message]
   }
 
   type Mutation {
     loginUser(email: String!, password: String!): Auth
     addUser(firstName: String!, lastName: String!, username: String!, email: String!, password: String!, company: String!): Auth
-    getCompany(name: String!, code: String!): Company
+    verifyCompanyCode(name: String!, code: String!): Company
     addMessage(messageData: MessageInput!): Room
+    getMessages(username: String!, text: String!, timeStamp: String!): Room
   }
 
 `;
