d8780620bab987f89acc4bc30b15404de658af14
Eric <esbev@hotmail.com>
2023-10-16 19:33:30 -0500

    got seeds to work

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,13 +1,9 @@
 const db = require('./connection');
 const { User, Company, Room } = require('../models');
-const cleanDB = require('./cleanDB');
 
 db.once('open', async () => {
-  await cleanDB('User', 'users');
-  await cleanDB('Company', 'companies');
-  await cleanDB('Room', 'rooms');
 
-  const rooms = await Room.insertMany([
+  await Room.insertMany([
     { name: 'Room1' },
     { name: 'Room2' },
     { name: 'Room3' },
@@ -19,62 +15,39 @@ db.once('open', async () => {
     { name: 'Room9' },
   ]);
 
+  const rooms = await Room.find();
+
   console.log('rooms seeded');
 
-  const companies = await Company.insertMany([
+ await Company.insertMany([
     { name: 'Company1', 
       code: 'joker', 
-      chatRooms: [rooms[0]._id, rooms[1]._id, rooms[2]._id] 
     },
     { name: 'Company2', 
       code: 'batman', 
-      chatRooms: [rooms[3]._id, rooms[4]._id, rooms[5]._id] 
     },
     { name: 'Company3', 
       code: 'riddler', 
-      chatRooms: [rooms[6]._id, rooms[7]._id, rooms[8]._id] 
     },
   ]);
 
+  await Promise.all([
+    { name: 'Company1', 
+      chatRooms: [rooms[0]._id, rooms[1]._id, rooms[2]._id] 
+    },
+    { name: 'Company2', 
+      chatRooms: [rooms[3]._id, rooms[4]._id, rooms[5]._id] 
+    },
+    { name: 'Company3', 
+      chatRooms: [rooms[6]._id, rooms[7]._id, rooms[8]._id] 
+    },
+  ].map(async ({name, chatRooms}) => {
+    await Company.updateMany(
+      {name: name, },
+      {chatRooms: chatRooms })
+  }));
+
   console.log('companies seeded');
-
-  await User.create({
-    firstName: 'Danfeng',
-    lastName: 'Wang',
-    username: 'danny',
-    email: 'danfeng@testmail.com',
-    password: 'password12345',
-    company: companies[0].name,
-  });
-
-  await User.create({
-    firstName: 'Rodolfo',
-    lastName: 'Silva',
-    username: 'morgan',
-    email: 'rodolfo@testmail.com',
-    password: 'password12345',
-    company: companies[0].name,
-  });
-
-  await User.create({
-    firstName: 'Alex',
-    lastName: 'Colwell',
-    username: 'alex',
-    email: 'allcold@testmail.com',
-    password: 'password12345',
-    company: companies[1].name,
-  });
-
-  await User.create({
-    firstName: 'Eric',
-    lastName: 'Beverly',
-    username: 'esbev',
-    email: 'ericb@testmail.com',
-    password: 'password12345',
-    company: companies[1].name,
-  });
-
-  console.log('users seeded');
 
   process.exit();
 });
