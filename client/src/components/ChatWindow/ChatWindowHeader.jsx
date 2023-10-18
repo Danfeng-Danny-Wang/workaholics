8b97dc8b497ff1475800dc642de3a9a85fa2ff22
Eric <esbev@hotmail.com>
2023-10-17 22:09:55 -0500

    chatwindow, chatwindow header

--- client/src/components/ChatWindow/ChatWindowHeader.jsx
+++ client/src/components/ChatWindow/ChatWindowHeader.jsx
@@ -1,12 +1,14 @@
 import { Grid, Typography, Divider } from "@mui/material";
-
 import LinkButton from "../LinkButton/LinkButton";
 
-function ChatWindowHeader({ user }) {
+function ChatWindowHeader({user}) {
+
+
+
   return (
     <>
       <Grid id="chat-header" xs={9} item>
-        <Typography variant="h6">{user} is in Chatroom 1</Typography>
+        <Typography variant="h6">{user} is in Chatroom</Typography>
       </Grid>
       <Grid id="chat-header" xs={1} item>
         <LinkButton url="/Lobby" variant="contained" color="warning">
